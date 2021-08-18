c4bf379636d7f81d4f0a1c97c3484118d908c49b
F904 <83501401+dF-F904@users.noreply.github.com>
2021-08-18 12:23:31 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), intents=intents, case_insensitive=True, help_command=None)
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+bot.run('ODQ1MjkwNjMzNDc2NTcxMTc2.YKe0Pw.-fFXBnNTL65CKYKiJv86KvdWnJs')
